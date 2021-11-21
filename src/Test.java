4683446896740d584334ac005b39232a1d964bd9
Boydya <b.s.chornovolenko@student.khai.edu>
2021-11-21 19:50:20 +0200

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Java education");
-=======
-        System.out.println("education");
-        //new string
-<<<<<<< HEAD
->>>>>>> 50307f0 (Test.java added to feature_branch with)
-=======
-        System.out.println("DICT");
->>>>>>> 82f5f81 (Test.java added to feature_branch with)
     }
 }
